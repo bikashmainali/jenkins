1130467d522b10a64993f64d9231e2bf5e38a72b
Bikash Mainali <bikash.mainali@regeneron.com>
2018-10-10 13:55:08 -0400

    previous didn't work. Trying it again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,9 +9,12 @@ public class App
     private static final String message = "Hello World!";
 
     public App() {
+        // little change
+        // this code is empty because ...
     }
 
     public static void main(String[] args) {
+        // just returns static variable value
         System.out.println(new App().getMessage());
     }
 
